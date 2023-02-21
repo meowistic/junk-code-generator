fa6954d450bc3a6304c180eeb07a849300375939
meowistic <126018790+meowistic@users.noreply.github.com>
2023-02-21 21:58:21 +0200

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,34 @@
+import random
+
+
+def rand():
+    w = random.randint(10, 35)
+    r = "abcdefghijklmnopqrstuvwxyz1234567890"
+    v = "".join(random.sample(r, w))
+    while True:
+        if v.startswith(("1","2","3","4","5","6","7","8","9","0")):
+            v = "".join(random.sample(r, w))
+        else:
+            return v
+ 
+def junk():          
+    k = random.choice((f"""def {rand()}():
+   {rand()} = "{rand()}"
+   for {rand()} in range({random.randint(9,835)}):
+       v = str("{rand()}")
+       {rand()} = v
+       {rand()} = {random.randint(5,946)}""", f"""class {rand()}():
+    def {rand()}():
+       {rand()} = "{rand()}"
+       for {rand()} in range({random.randint(9,835)}):
+           v = str("{rand()}")
+           {rand()} = v
+           {rand()} = {random.randint(5,946)}
+           {rand()} = [{random.randint(500,93949)}, "{rand()}", "{rand()}"]""", f"""{rand()} = '{rand()}'""", f"""{rand()} = {random.randint(10000, 999999)}
+{rand()} = {random.randint(10000, 999999)}
+{rand()} = {random.randint(10000, 999999)}
+{rand()} = {random.randint(10000, 999999)}"""))
+    return k
+           
+    
+input(f'{junk()}\n{junk()}\n{junk()}\n{junk()}\n{junk()}\n # func_255ab\n{junk()}\n# edit value {random.randint(100, 1500)} ASAP.\n{junk()}\n{junk()}\n{junk()}\n{junk()}\n{junk()}\n{junk()}')
